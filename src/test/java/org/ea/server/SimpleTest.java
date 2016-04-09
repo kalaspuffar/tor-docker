6c3f1e28f72a061e7a222cd0458b1009b73073f2
Daniel Persson <mailto.woden@gmail.com>
2016-04-09 20:04:52 +0200

    Added test

---
+++ src/test/java/org/ea/server/SimpleTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MyTests { 
+  @Test 
+  public void isTrue() { 
+    assertTrue("Must be true", true);
+  
+  } 
+} 
