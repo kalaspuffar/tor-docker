7f39177e9dbc1a9ccdfdec16d799c651f4dc7cd3
Daniel Persson <mailto.woden@gmail.com>
2016-04-15 22:40:36 +0200

    Todays work

--- src/test/java/org/ea/server/SimpleTest.java
+++ src/test/java/org/ea/server/SimpleTest.java
@@ -2,7 +2,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-public class MyTests { 
+public class SimpleTest { 
   @Test 
   public void isTrue() { 
     assertTrue("Must be true", true);
