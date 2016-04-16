0c29d8c682a7f0689392f9fc8506d9e655557e46
Daniel Persson <mailto.woden@gmail.com>
2016-04-16 14:42:08 +0200

    Added server http

--- src/main/java/org/ea/server/TestProxy.java
+++ src/main/java/org/ea/server/TestProxy.java
@@ -5,6 +5,14 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import java.nio.file.Files;
 
+import com.sun.net.httpserver.HttpExchange; 
+import com.sun.net.httpserver.HttpHandler; 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 import com.msopentech.thali.toronionproxy.OnionProxyContext;
 import com.msopentech.thali.toronionproxy.OnionProxyManager;
 import com.msopentech.thali.toronionproxy.Utilities;
@@ -26,14 +34,27 @@ public class TestProxy {
             return;
         }
 
-        // Start a hidden service listener
+        HttpServer server = HttpServer.create(new InetSocketAddress(65000), 0);
+     		  server.createContext("/", new MyHandler());
+     		  server.setExecutor(null);
+     		  
+     		  server.start();
+
         int hiddenServicePort = 9797;
-        int localPort = 80;
+        int localPort = 65000;
         String onionAddress = onionProxyManager.publishHiddenService(hiddenServicePort, localPort);
 
         System.out.println(onionAddress);
         
-        
         Thread.sleep(30 * 60 * 1000);
     }
+     		 
+    static class MyHandler implements HttpHandler {
+      @Override public void handle(HttpExchange t) throws IOException {
+      	String response = "";
+      	t.sendResponseHeaders(200, response.length());
+      	OutputStream os = t.getResponseBody(); os.write(response.getBytes()); os.close();
+      	
+      	}
+   }
 }
